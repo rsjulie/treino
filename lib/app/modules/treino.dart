1bb8f5445d2ee7eea54f8343cdd5cfa6ba4fa78b
Julie <julie.ribeiro.silva@gmail.com>
2022-05-06 13:32:10 -0300

    home

--- lib/app/modules/treino.dart
+++ lib/app/modules/treino.dart
@@ -1,5 +1,3 @@
-import 'dart:ui' as ui;
-
 import 'package:flutter/material.dart';
 
 class Treino extends StatelessWidget {
@@ -23,6 +21,12 @@ class Treino extends StatelessWidget {
           children: [
             const SizedBox(height: 15),
             Card(
+              shape: RoundedRectangleBorder(
+                side: const BorderSide(
+                    color: Color.fromARGB(179, 255, 0, 0), width: 2),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              color: const Color(0x1FFFFFFF),
               margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
               elevation: 2,
               child: InkWell(
@@ -30,74 +34,112 @@ class Treino extends StatelessWidget {
                   Navigator.pushNamed(context, '/treinoA');
                 },
                 child: const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 80),
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 75),
                   child: Center(
                       child: Text(
                     'Treino A',
-                    style: TextStyle(
-                      fontSize: 22,
-                    ),
+                    style: TextStyle(fontSize: 28, color: Colors.white),
                   )),
                 ),
               ),
             ),
             Card(
+              shape: RoundedRectangleBorder(
+                side: const BorderSide(
+                    color: Color.fromARGB(179, 255, 0, 0), width: 2),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              color: const Color(0x1FFFFFFF),
               elevation: 2,
               margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
               child: InkWell(
                 onTap: () {},
                 child: const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 80),
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 75),
                   child: Center(
                       child: Text(
                     'Treino B',
-                    style: TextStyle(
-                      fontSize: 22,
-                    ),
+                    style: TextStyle(fontSize: 28, color: Colors.white),
                   )),
                 ),
               ),
             ),
-            Stack(
-              children: [
-                Container(
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      image: NetworkImage(
-                          'https://media.istockphoto.com/photos/hd-wallpaper-for-mobile-phones-picture-id1257005098?b=1&k=20&m=1257005098&s=170667a&w=0&h=je8sgDZ9o62xavDKDHVSaAE4M6wug7Yt5vOaPI0VHFo='),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  //I blured the parent container to blur background image, you can get rid of this part
-                  child: BackdropFilter(
-                    filter: ui.ImageFilter.blur(sigmaX: 3.0, sigmaY: 3.0),
-                    child: Container(
-                      //you can change opacity with color here(I used black) for background.
-                      decoration:
-                          BoxDecoration(color: Colors.black.withOpacity(0.2)),
-                    ),
-                  ),
-                ),
-                Card(
-                  color: Colors.transparent,
-                  elevation: 2,
-                  margin:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  child: InkWell(
+            Card(
+              shape: RoundedRectangleBorder(
+                side: const BorderSide(
+                    color: Color.fromARGB(179, 255, 0, 0), width: 2),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              color: const Color(0x1FFFFFFF),
+              elevation: 2,
+              margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Stack(
+                children: [
+                  InkWell(
                     onTap: () {},
                     child: const Padding(
                       padding:
-                          EdgeInsets.symmetric(horizontal: 15, vertical: 80),
+                          EdgeInsets.symmetric(horizontal: 15, vertical: 75),
                       child: Center(
                           child: Text(
                         'Treino C - aeróbico',
-                        style: TextStyle(fontSize: 22, color: Colors.white),
+                        style: TextStyle(fontSize: 28, color: Colors.white),
                       )),
                     ),
                   ),
-                ),
-              ],
+                  Container(
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: NetworkImage(
+                              'https://media.istockphoto.com/photos/hd-wallpaper-for-mobile-phones-picture-id1257005098?b=1&k=20&m=1257005098&s=170667a&w=0&h=je8sgDZ9o62xavDKDHVSaAE4M6wug7Yt5vOaPI0VHFo='),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      //I blured the parent container to blur background image, you can get rid of this part
+                      child: Container()),
+                ],
+              ),
             ),
+            // Stack(
+            //   children: [
+            //     Container(
+            //       decoration: const BoxDecoration(
+            //         image: DecorationImage(
+            //           image: NetworkImage(
+            //               'https://media.istockphoto.com/photos/hd-wallpaper-for-mobile-phones-picture-id1257005098?b=1&k=20&m=1257005098&s=170667a&w=0&h=je8sgDZ9o62xavDKDHVSaAE4M6wug7Yt5vOaPI0VHFo='),
+            //           fit: BoxFit.cover,
+            //         ),
+            //       ),
+            //       //I blured the parent container to blur background image, you can get rid of this part
+            //       child: BackdropFilter(
+            //         filter: ui.ImageFilter.blur(sigmaX: 3.0, sigmaY: 3.0),
+            //         child: Container(
+            //           //you can change opacity with color here(I used black) for background.
+            //           decoration:
+            //               BoxDecoration(color: Colors.black.withOpacity(0.2)),
+            //         ),
+            //       ),
+            //     ),
+            //     Card(
+            //       color: Colors.transparent,
+            //       elevation: 2,
+            //       margin:
+            //           const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            //       child: InkWell(
+            //         onTap: () {},
+            //         child: const Padding(
+            //           padding:
+            //               EdgeInsets.symmetric(horizontal: 15, vertical: 75),
+            //           child: Center(
+            //               child: Text(
+            //             'Treino C - aeróbico',
+            //             style: TextStyle(fontSize: 22, color: Colors.white),
+            //           )),
+            //         ),
+            //       ),
+            //     ),
+            //   ],
+            // ),
           ],
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.purple,
+        primarySwatch: Colors.red,
       ),
       home: const Treino(),
       routes: {
